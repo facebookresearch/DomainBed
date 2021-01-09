56196cc5f8a8d53d3d7f547e385c443cbcb73dcd
Ishaan Gulrajani <igul222@gmail.com>
2021-01-09 16:03:03 -0500

    Experimental multi-GPU launcher

--- domainbed/command_launchers.py
+++ domainbed/command_launchers.py
@@ -7,6 +7,8 @@ which runs all commands serially on the local machine.
 """
 
 import subprocess
+import time
+import torch
 
 def local_launcher(commands):
     """Launch commands serially on the local machine."""
@@ -14,14 +16,42 @@ def local_launcher(commands):
         subprocess.call(cmd, shell=True)
 
 def dummy_launcher(commands):
-    """Doesn't run anything; instead, prints each command.
-    Useful for testing."""
+    """
+    Doesn't run anything; instead, prints each command.
+    Useful for testing.
+    """
     for cmd in commands:
         print(f'Dummy launcher: {cmd}')
 
+def multi_gpu_launcher(commands):
+    """
+    Launch commands on the local machine, using all GPUs in parallel.
+    """
+    print('WARNING: using experimental multi_gpu_launcher.')
+    n_gpus = torch.cuda.device_count()
+    procs_by_gpu = [None]*n_gpus
+
+    while len(commands) > 0:
+        for gpu_idx in range(n_gpus):
+            proc = procs_by_gpu[gpu_idx]
+            if (proc is None) or (proc.poll() is not None):
+                # Nothing is running on this GPU; launch a command.
+                cmd = commands.pop(0)
+                new_proc = subprocess.Popen(
+                    f'CUDA_VISIBLE_DEVICES={gpu_idx} {cmd}', shell=True)
+                procs_by_gpu[gpu_idx] = new_proc
+                break
+        time.sleep(1)
+
+    # Wait for the last few tasks to finish before returning
+    for p in procs_by_gpu:
+        if p is not None:
+            p.wait()
+
 REGISTRY = {
     'local': local_launcher,
-    'dummy': dummy_launcher
+    'dummy': dummy_launcher,
+    'multi_gpu': multi_gpu_launcher
 }
 
 try:
