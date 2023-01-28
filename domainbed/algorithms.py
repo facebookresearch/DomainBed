22002eaae419ddb30c911b40c40bd27513baebb9
Cian Eastwood <s1668298@talorc.inf.ed.ac.uk>
2023-01-28 11:26:53 +0000

    Added EQRM.

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -17,7 +17,7 @@ except:
 from domainbed import networks
 from domainbed.lib.misc import (
     random_pairs_of_minibatches, split_meta_train_test, ParamDict,
-    MovingAverage, l2_between_dicts, proj
+    MovingAverage, l2_between_dicts, proj, Nonparametric
 )
 
 
@@ -51,6 +51,7 @@ ALGORITHMS = [
     'Transfer',
     'CausIRL_CORAL',
     'CausIRL_MMD',
+    'EQRM',
 ]
 
 def get_algorithm_class(algorithm_name):
@@ -1987,3 +1988,48 @@ class CausIRL_CORAL(AbstractCausIRL):
     def __init__(self, input_shape, num_classes, num_domains, hparams):
         super(CausIRL_CORAL, self).__init__(input_shape, num_classes, num_domains,
                                   hparams, gaussian=False)
+
+
+class EQRM(ERM):
+    """
+    Empirical Quantile Risk Minimization (EQRM).
+    Algorithm 1 from [https://arxiv.org/pdf/2207.09944.pdf].
+    """
+    def __init__(self, input_shape, num_classes, num_domains, hparams, dist=None):
+        super().__init__(input_shape, num_classes, num_domains, hparams)
+        self.register_buffer('update_count', torch.tensor([0]))
+        self.register_buffer('alpha', torch.tensor(self.hparams["eqrm_quantile"], dtype=torch.float64))
+        if dist is None:
+            self.dist = Nonparametric()
+        else:
+            self.dist = dist
+
+    def risk(self, x, y):
+        return F.cross_entropy(self.network(x), y).reshape(1)
+
+    def update(self, minibatches, unlabeled=None):
+        env_risks = torch.cat([self.risk(x, y) for x, y in minibatches])
+
+        if self.update_count < self.hparams["eqrm_burnin_iters"]:
+            # Burn-in/annealing period uses ERM like penalty methods (which set penalty_weight=0, e.g. IRM, VREx.)
+            loss = torch.mean(env_risks)
+        else:
+            # Loss is the alpha-quantile value
+            self.dist.estimate_parameters(env_risks)
+            loss = self.dist.icdf(self.alpha)
+
+        if self.update_count == self.hparams['eqrm_burnin_iters']:
+            # Reset Adam (like IRM, VREx, etc.), because it doesn't like the sharp jump in
+            # gradient magnitudes that happens at this step.
+            self.optimizer = torch.optim.Adam(
+                self.network.parameters(),
+                lr=self.hparams["eqrm_lr"],
+                weight_decay=self.hparams['weight_decay'])
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        self.update_count += 1
+
+        return {'loss': loss.item()}
--- domainbed/hparams_registry.py
+++ domainbed/hparams_registry.py
@@ -136,6 +136,11 @@ def _hparams(algorithm, dataset, random_seed):
         _hparam('beta1', 0.5, lambda r: r.choice([0., 0.5]))
         _hparam('lr_d', 1e-3, lambda r: 10**r.uniform(-4.5, -2.5))
 
+    elif algorithm == 'EQRM':
+        _hparam('eqrm_quantile', 0.75, lambda r: r.uniform(0.5, 0.99))
+        _hparam('eqrm_burnin_iters', 2500, lambda r: 10 ** r.uniform(2.5, 3.5))
+        _hparam('eqrm_lr', 1e-6, lambda r: 10 ** r.uniform(-7, -5))
+
 
     # Dataset-and-algorithm-specific hparam definitions. Each block of code
     # below corresponds to exactly one hparam. Avoid nested conditionals.
