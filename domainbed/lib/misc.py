68be2ec3111aae6c405b23ee270f377c08c35f82
kimathikaai <kiprotich.kaai@gmail.com>
2023-02-23 00:12:44 -0500

    fixup! Including additional tracking metrics

--- domainbed/lib/misc.py
+++ domainbed/lib/misc.py
@@ -278,7 +278,7 @@ def accuracy(network, loader, weights, device, dataset):
 
     other_acc = correct / total
 
-    assert other_acc == compute_acc, f"{other_acc}, {compute_acc}"
+    assert np.isclose(other_acc, compute_acc, atol=1e-06), f"{other_acc}, {compute_acc}"
 
     return compute_acc, compute_f1, overlap_class_acc, non_overlap_class_acc
 
