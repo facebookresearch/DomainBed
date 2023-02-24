a9618f0e8dc104178f770687cd7a14901b136142
kimathikaai <kiprotich.kaai@gmail.com>
2023-02-24 17:37:15 -0500

    Update scripts

--- sweep.sh
+++ sweep.sh
@@ -3,18 +3,26 @@
 # local variables
 outputdir=/pub2/podg
 datadir=/pub2/data
+algorithm=ERM
+n_hparams=5
+steps=2100
+trial=3
 
 jobid=$(date +"%y%m%d%H%M%S")
-echo starting ${jobid}
-mkdir ${outputdir}/${jobid}
+overlap=100
+datasets=VLCS
+current_output_dir=${outputdir}/${jobid}_${algorithm}_${datasets}_o${overlap}_h${n_hparams}_s${steps}_t${trial}
+echo starting ${current_output_dir}
+mkdir current_output_dir
+
 CUDA_VISIBLE_DEVICES=2 python -m domainbed.scripts.sweep launch\
        --data_dir=${datadir} \
-       --output_dir=${outputdir}/${jobid} \
+       --output_dir=${current_output_dir} \
        --command_launcher local \
-       --overlap 66 \
-       --steps 2100 \
+       --overlap ${overlap} \
+       --steps ${steps} \
        --single_test_envs \
-       --algorithms ERM \
-       --datasets VLCS \
-       --n_hparams 5\
-       --n_trials 3
+       --algorithms ${algorithm} \
+       --datasets ${datasets} \
+       --n_hparams ${n_hparams} \
+       --n_trials ${trial}
