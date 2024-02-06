5f805e387e97441c5a901c09cba3b69f8432bdee
Lê Nam Khánh <55955273+khanhkhanhlele@users.noreply.github.com>
2024-02-06 14:38:07 +0700

    Add CAG algorithm implementation

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -52,6 +52,7 @@ ALGORITHMS = [
     'CausIRL_CORAL',
     'CausIRL_MMD',
     'EQRM',
+    'CAG' ,
 ]
 
 def get_algorithm_class(algorithm_name):
@@ -106,9 +107,7 @@ class ERM(Algorithm):
         )
 
     def update(self, minibatches, unlabeled=None):
-        print(len(minibatches))
         all_x = torch.cat([x for x, y in minibatches])
-        print(all_x.shape)
         all_y = torch.cat([y for x, y in minibatches])
         loss = F.cross_entropy(self.predict(all_x), all_y)
 
@@ -181,6 +180,64 @@ class Fish(Algorithm):
     def predict(self, x):
         return self.network(x)
 
+class CAG(Algorithm):
+    """
+    Implementation of Fish, as seen in Gradient Matching for Domain
+    Generalization, Shi et al. 2021.
+    """
+
+    def __init__(self, input_shape, num_classes, num_domains, hparams):
+        super(Fish, self).__init__(input_shape, num_classes, num_domains,
+                                   hparams)
+        self.input_shape = input_shape
+        self.num_classes = num_classes
+
+        self.network = networks.WholeFish(input_shape, num_classes, hparams)
+        self.optimizer = torch.optim.Adam(
+            self.network.parameters(),
+            lr=self.hparams["lr"],
+            weight_decay=self.hparams['weight_decay']
+        )
+        self.optimizer_inner_state = None
+
+    def create_clone(self, device):
+        self.network_inner = networks.WholeFish(self.input_shape, self.num_classes, self.hparams,
+                                            weights=self.network.state_dict()).to(device)
+        self.optimizer_inner = torch.optim.Adam(
+            self.network_inner.parameters(),
+            lr=self.hparams["lr"],
+            weight_decay=self.hparams['weight_decay']
+        )
+        if self.optimizer_inner_state is not None:
+            self.optimizer_inner.load_state_dict(self.optimizer_inner_state)
+
+    def fish(self, meta_weights, inner_weights, lr_meta):
+        meta_weights = ParamDict(meta_weights)
+        inner_weights = ParamDict(inner_weights)
+        meta_weights += lr_meta * (inner_weights - meta_weights)
+        return meta_weights
+
+    def update(self, minibatches, unlabeled=None):
+        self.create_clone(minibatches[0][0].device)
+
+        for x, y in minibatches:
+            loss = F.cross_entropy(self.network_inner(x), y)
+            self.optimizer_inner.zero_grad()
+            loss.backward()
+            self.optimizer_inner.step()
+
+        self.optimizer_inner_state = self.optimizer_inner.state_dict()
+        meta_weights = self.fish(
+            meta_weights=self.network.state_dict(),
+            inner_weights=self.network_inner.state_dict(),
+            lr_meta=self.hparams["meta_lr"]
+        )
+        self.network.reset_weights(meta_weights)
+
+        return {'loss': loss.item()}
+
+    def predict(self, x):
+        return self.network(x)
 
 class ARM(ERM):
     """ Adaptive Risk Minimization (ARM) """
