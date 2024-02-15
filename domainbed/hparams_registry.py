9d8e9bd28e8068d675a435f495436191c1e25df4
Lê Nam Khánh <55955273+khanhkhanhlele@users.noreply.github.com>
2024-02-15 19:15:24 +0700

    Add CAG algorithm hyperparameters

--- domainbed/hparams_registry.py
+++ domainbed/hparams_registry.py
@@ -52,7 +52,10 @@ def _hparams(algorithm, dataset, random_seed):
         _hparam('meta_lr', 0.5, lambda r:r.choice([0.05, 0.1, 0.5]))
     
     elif algorithm == 'CAG':
-        _hparam('meta_lr', 0.5, lambda r:r.choice([0.05, 0.1, 0.5]))
+        _hparam('meta_lr', 0.5, lambda r: r.choice([0.05, 0.1, 0.5]))
+        _hparam('alpha', 0.01, lambda r: r.choice([0.01, 0.05, 0.1]))
+        _hparam('cag_update', 30, lambda r: r.choice([1, 5, 10]))
+        _hparam('cag_epoch', 15, lambda r: r.choice([10, 20, 30]))
 
     elif algorithm == "RSC":
         _hparam('rsc_f_drop_factor', 1/3, lambda r: r.uniform(0, 0.5))
