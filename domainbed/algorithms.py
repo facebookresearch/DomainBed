d6a79ae073e57cb9d3fe52b63420d672255931ca
Lê Nam Khánh <55955273+khanhkhanhlele@users.noreply.github.com>
2024-02-16 21:57:16 +0700

    d

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -248,8 +248,7 @@ class CAG(Algorithm):
         meta_weights = ParamDict(meta_weights.state_dict())
         for i_domain in range (self.num_domains):
             domain_grad = ParamDict(inner_weights[i_domain].state_dict()) - meta_weights
-            for _, para in domain_grad.items():
-                print(para)
+            print(domain_grad)
         # meta_grad = self.cagrad(domain_grad, self.num_domains)
         # meta_weights += lr_meta * meta_grad
         return meta_weights 
