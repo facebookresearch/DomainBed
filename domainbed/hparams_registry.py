876c08caa7367c39eb3e27ed625daae928ecbbbb
Lê Nam Khánh <55955273+khanhkhanhlele@users.noreply.github.com>
2024-02-18 15:30:14 +0700

    s

--- domainbed/hparams_registry.py
+++ domainbed/hparams_registry.py
@@ -57,6 +57,12 @@ def _hparams(algorithm, dataset, random_seed):
         _hparam('cag_update', 1, lambda r: r.choice([1, 5, 10]))
         # _hparam('cag_epoch', 15, lambda r: r.choice([10, 20, 30]))
 
+    elif algorithm == 'Gradbase':
+        _hparam('meta_lr', 0.5, lambda r: r.choice([0.05, 0.1, 0.5]))
+        # _hparam('alpha', 0.01, lambda r: r.choice([0.01, 0.05, 0.1]))
+        _hparam('grad_update', 1, lambda r: r.choice([1, 5, 10]))
+        # _hparam('cag_epoch', 15, lambda r: r.choice([10, 20, 30]))
+        
     elif algorithm == "RSC":
         _hparam('rsc_f_drop_factor', 1/3, lambda r: r.uniform(0, 0.5))
         _hparam('rsc_b_drop_factor', 1/3, lambda r: r.uniform(0, 0.5))
