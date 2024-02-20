c623d6abe2e03f4076cbb5a64bb8cd3c4677d78c
Lê Nam Khánh <55955273+khanhkhanhlele@users.noreply.github.com>
2024-02-20 16:15:50 +0700

    s

--- domainbed/scripts/train.py
+++ domainbed/scripts/train.py
@@ -77,6 +77,7 @@ if __name__ == "__main__":
     else:
         hparams = hparams_registry.random_hparams(args.algorithm, args.dataset,
             misc.seed_hash(args.hparams_seed, args.trial_seed))
+        print(hparams)
     if args.hparams:
         hparams.update(json.loads(args.hparams))
 
