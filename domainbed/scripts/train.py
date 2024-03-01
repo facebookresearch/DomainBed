340c7179376591ba0c4e182740e240920064cd2e
Lê Nam Khánh <55955273+khanhkhanhlele@users.noreply.github.com>
2024-03-01 15:05:49 +0700

    f

--- domainbed/scripts/train.py
+++ domainbed/scripts/train.py
@@ -95,7 +95,9 @@ if __name__ == "__main__":
     if args.hparams:
         hparams.update(json.loads(args.hparams))
         
-    # Setup
+    # Setup wandb
+    print(args)
+    print(hparams)
     log_interface = Logging(args, hparams)
 
     print('HParams:')
@@ -265,50 +267,10 @@ if __name__ == "__main__":
             with open(epochs_path, 'a') as f:
                 f.write(json.dumps(results, sort_keys=True) + "\n")
             
-            # def check_and_convert(value):
-            #     """
-            #     Kiểm tra và chuyển đổi giá trị nếu cần.
-            #     Trả về giá trị đã chuyển đổi và flag chỉ ra liệu có lỗi không.
-            #     """
-            #     if isinstance(value, np.integer):
-            #         return int(value), False
-            #     elif isinstance(value, (list, tuple)):
-            #         return [check_and_convert(v)[0] for v in value], False
-            #     elif isinstance(value, dict):
-            #         return {k: check_and_convert(v)[0] for k, v in value.items()}, False
-            #     else:
-            #         return value, False
-
-            # def validate_and_report(results):
-            #     """
-            #     Duyệt qua results và báo cáo nếu có giá trị không thể serialize bằng JSON.
-            #     """
-            #     errors = []
-            #     for key, value in results.items():
-            #         _, error = check_and_convert(value)
-            #         if error:
-            #             errors.append(key)
-                
-            #     if errors:
-            #         print(f"Có lỗi ở các phần tử sau: {errors}")
-            #     # else:
-            #         # print("Tất cả các phần tử đều có thể serialize bằng JSON.")
-            #     return errors
-
-            # # Kiểm tra và in ra báo cáo
-            # errors = validate_and_report(results)
-
-            # # Nếu không có lỗi, ghi vào file
-            # if not errors:
-            #     with open('epochs_path.json', 'a') as f:
-            #         f.write(json.dumps(results, default=lambda o: check_and_convert(o)[0], sort_keys=True) + "\n")
-            # else:
-            #     print("Đã xảy ra lỗi trong quá trình kiểm tra, không ghi vào file.")
-            
+            #logging
             for key, value in results.items():
                 if (key != "hparams") and (key != "args"):
                     log_interface(key=f"test/{key}", value=value)
-
             log_interface.step(epoch=int(step//checkpoint_freq), test_len=int(n_steps//checkpoint_freq))
 
             algorithm_dict = algorithm.state_dict()
