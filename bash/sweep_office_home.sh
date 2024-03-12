e208b59a831a002374086265f51ff74e2f4300a8
Lê Nam Khánh <55955273+khanhkhanhlele@users.noreply.github.com>
2024-03-12 13:05:21 +0700

    s

--- bash/sweep_office_home.sh
+++ bash/sweep_office_home.sh
@@ -5,7 +5,7 @@ python -m domainbed.scripts.sweep launch\
        --command_launcher local\
        --algorithms CAG1 \
        --datasets OfficeHome \
-       --n_hparams 20\
+       --n_hparams 1\
        --n_trials 3\
        --skip_confirmation\
        --wandb\
