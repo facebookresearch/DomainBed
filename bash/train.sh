6b44c8b15120b0594b0a2ef1bf494fa768df182e
namkhanh <namkhanh2172@gmail.com>
2024-03-13 10:37:44 +0700

    a

--- bash/train.sh
+++ bash/train.sh
@@ -2,18 +2,9 @@
 # CUDA_VISIBLE_DEVICES=1 \
 python3 -m domainbed.scripts.train\
        --data_dir=./domainbed/data/\
-<<<<<<< HEAD
        --algorithm CAG1\
        --dataset DomainNet\
        --test_env 0\
        --steps 2\
        --hparams_seed 0\
        --trial_seed 0\
-=======
-       --algorithm CAG\
-       --dataset PACS\
-       --test_env 0\
-       --steps 2\
-       # --hparams_seed 1\
-       # --trial_seed 1\
->>>>>>> effba743229bcf367223ed008cb92247b541f99d
