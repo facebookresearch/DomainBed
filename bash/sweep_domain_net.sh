b6b997396f44b13724f678b255943b2be79176b1
namkhanh <namkhanh2172@gmail.com>
2024-03-13 10:38:55 +0700

    r

---
+++ bash/sweep_domain_net.sh
@@ -0,0 +1,10 @@
+python -m domainbed.scripts.sweep launch\
+       --data_dir=./domainbed/data/\
+       --output_dir=./train_output\
+       --command_launcher local\
+       --algorithms CAG1 \
+       --datasets PACS \
+       --n_hparams 1\
+       --n_trials 3\
+       --skip_confirmation\
+       --wandb\
