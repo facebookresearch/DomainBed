4ff58b54df31953be39e53595ae7e0769cce829e
namkhanh <namkhanh2172@gmail.com>
2024-03-13 10:53:25 +0700

    f

--- bash/sweep.sh
+++ bash/sweep.sh
@@ -3,7 +3,7 @@
 # RotatedMNIST ColoredMNIST
 # ERM GradBase CAG Fish
 CUDA_VISIBLE_DEVICES=1\ 
-python3 -m domainbed.scripts.sweep delete_incomplete\
+python3 -m domainbed.scripts.sweep launch\
        --data_dir=./domainbed/data/MNIST/\
        --output_dir=./train_output\
        --command_launcher local\
