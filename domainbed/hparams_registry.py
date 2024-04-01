be4938bfabee461ef772cb14d53863e1b3ebcee0
baochi0212 <chideptraiak29@gmail.com>
2024-04-01 08:16:10 +0000

    Update training and sweeping parameters

--- domainbed/hparams_registry.py
+++ domainbed/hparams_registry.py
@@ -52,8 +52,8 @@ def _hparams(algorithm, dataset, random_seed):
         _hparam('meta_lr', 0.5, lambda r:r.choice([0.05, 0.1, 0.5]))
         
     elif algorithm == 'CAG' or algorithm == 'CAG1':
-        _hparam('meta_lr', 0.5, lambda r: r.choice([0.01, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0]))
-        _hparam('cagrad_c', 0.5, lambda r: r.choice([0.01, 0.05, 0.1, 0,2, 0.5, 1.0, 2.0, 5.0]))
+        _hparam('meta_lr', 0.5, lambda r: r.choice([0.01, 0.05, 0.1, 0.2, 0.5, 0.8, 1.0, 2.0, 5.0]))
+        _hparam('cagrad_c', 0.5, lambda r: r.choice([0.01, 0.05, 0.1, 0,2, 0.5, 0.8, 1.0, 2.0, 5.0]))
         _hparam('cag_update', 1, lambda r: int(r.choice([1])))
 
     elif algorithm == 'GradBase':
