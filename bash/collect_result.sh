62fb6a8723a7f42d128ad12200c5682ad377b29e
namkhanh <namkhanh2172@gmail.com>
2024-04-04 13:33:27 +0700

    Update sweep parameters and collect results

--- bash/collect_result.sh
+++ bash/collect_result.sh
@@ -1,5 +1,5 @@
 # python domainbed/scripts/collect_results_wandb.py
 
 python -m domainbed.scripts.collect_results\
-       --input_dir=./MnistTest\
-       # --latex
+       --input_dir=./train_output\
+       --latex
