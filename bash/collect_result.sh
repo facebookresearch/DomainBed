a98834e93272be1d123a73cf0f855eaf12e5014d
baochi0212 <chideptraiak29@gmail.com>
2024-04-17 14:02:48 +0000

    Update sweep parameters and collect results

--- bash/collect_result.sh
+++ bash/collect_result.sh
@@ -1,12 +1,12 @@
 # python domainbed/scripts/collect_results_wandb.py
 
-python -m domainbed.scripts.collect_results\
-       --input_dir=./train_output
+# python -m domainbed.scripts.collect_results\
+#        --input_dir=./train_output
 #        --latex
 
-python -m domainbed.scripts.collect_results\
-       --input_dir=./MnistTest 
+# python -m domainbed.scripts.collect_results\
+#        --input_dir=./MnistTest 
 #       --latex
 
-# python -m domainbed.scripts.collect_results_by_para\
-#        --input_dir=./MnistTest 
+python -m domainbed.scripts.collect_results_by_para\
+       --input_dir=./train_output 
--- domainbed/scripts/collect_results_by_para.py
+++ domainbed/scripts/collect_results_by_para.py
@@ -29,6 +29,7 @@ def plot_data_with_error_bars(data_dict, file_path):
     if not os.path.exists(file_path):
         os.makedirs(file_path)
     hparams_values, means, errors = zip(*data_dict['value_mean_err'])
+    print(data_dict['algorithm'], data_dict['hparams_key'],data_dict['dataset'], data_dict["selection_method"], hparams_values, means, errors)
     # Plotting the graph
     plt.figure(figsize=(8, 5))
     plt.errorbar(hparams_values, means, yerr=errors, fmt='-o', capsize=5)
@@ -66,17 +67,12 @@ def print_table(table, header_text, row_labels, col_labels, colwidth=10):
 
 def print_results_tables(records, selection_method):
     """Given all records, print a results table for each dataset."""
-    grouped_records = reporting.get_grouped_records(records).map(lambda group:
-        { **group, "sweep_acc": selection_method.sweep_acc(group["records"]) }
-    ).filter(lambda g: g["sweep_acc"] is not None)
-    
     #read hparams and sort (lexicographic order)
     cag_update = Q(records).select("hparams.cag_update").unique().sorted()
     cagrad_c = Q(records).select("hparams.cagrad_c").unique().sorted()
     meta_lr = Q(records).select("hparams.meta_lr").unique().sorted()
-    
     hparams_dict = {"cag_update": cag_update, "cagrad_c": cagrad_c, "meta_lr": meta_lr}
-
+    
     # read algorithm names and sort (predefined order)
     alg_names = Q(records).select("args.algorithm").unique()
     alg_names = ([n for n in algorithms.ALGORITHMS if n in alg_names] +
@@ -85,25 +81,25 @@ def print_results_tables(records, selection_method):
     # read dataset names and sort (lexicographic order)
     dataset_names = Q(records).select("args.dataset").unique().sorted()
     dataset_names = [d for d in datasets.DATASETS if d in dataset_names]
-
-    table = [[None for _ in [*dataset_names]] for _ in alg_names]
-    for i, algorithm in enumerate(alg_names):
-        for j, dataset in enumerate(dataset_names):
+    
+    for algorithm in alg_names:
+        for dataset in dataset_names:
             for hparams_key, hparams_value in hparams_dict.items():
                 value_mean_err=[]
                 for hparam in hparams_value:
-                    trial_averages = (grouped_records
-                        .filter_equals(f"algorithm, dataset,{hparams_key}", (algorithm, dataset,hparam))
+                    grouped_records = (reporting
+                        .get_grouped_records(records, hparams_key, hparam)
+                        .map(lambda group: { **group, "sweep_acc": selection_method.sweep_acc(group["records"]) })
+                        .filter(lambda g: g["sweep_acc"] is not None)
+                        .filter_equals(f"algorithm, dataset", (algorithm, dataset))
                         .group("trial_seed")
-                        .map(lambda trial_seed, group:
-                            group.select("sweep_acc").mean()
-                        )
+                        .map(lambda trial_seed, group:group.select("sweep_acc").mean())
                     )
-                    mean, err = format_mean(trial_averages)
+                    mean, err = format_mean(grouped_records)
                     value_mean_err.append((hparam, mean, err))
-                dict = {"algorithm": algorithm, "dataset": dataset, "hparams_key": hparams_key, "value_mean_err": value_mean_err}
+                dict = {"algorithm": algorithm, "dataset": dataset, "hparams_key": hparams_key, "value_mean_err": value_mean_err, "selection_method": selection_method.short_name}
                 plot_data_with_error_bars(dict,"./domainbed/results/plot/{}/{}/{}/{}/"
-                                          .format(algorithm, dataset, selection_method.short_name, hparams_key))
+                                        .format(algorithm, dataset, selection_method.short_name, hparams_key))
                 
        
 
