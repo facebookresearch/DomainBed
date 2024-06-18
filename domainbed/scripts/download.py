df74aa38942c9e2c218b6faac718a129550a0084
Piotr Teterwark <piotrt@scc-fb3.scc.bu.edu>
2024-06-18 09:25:04 -0400

    Fix TerraIncognita download

--- domainbed/scripts/download.py
+++ domainbed/scripts/download.py
@@ -1,5 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
+from collections import defaultdict
 from torchvision.datasets import MNIST
 import xml.etree.ElementTree as ET
 from zipfile import ZipFile
@@ -172,12 +173,14 @@ def download_terra_incognita(data_dir):
     full_path = stage_path(data_dir, "terra_incognita")
 
     download_and_extract(
-        "https://lilablobssc.blob.core.windows.net/caltechcameratraps/eccv_18_all_images_sm.tar.gz",
+        "https://storage.googleapis.com/public-datasets-lila/caltechcameratraps/eccv_18_all_images_sm.tar.gz",
         os.path.join(full_path, "terra_incognita_images.tar.gz"))
 
+
     download_and_extract(
-        "https://lilablobssc.blob.core.windows.net/caltechcameratraps/labels/caltech_camera_traps.json.zip",
-        os.path.join(full_path, "caltech_camera_traps.json.zip"))
+        "https://storage.googleapis.com/public-datasets-lila/caltechcameratraps/eccv_18_annotations.tar.gz",
+        os.path.join(full_path, "eccv_18_annotations.tar.gz"))
+
 
     include_locations = ["38", "46", "100", "43"]
 
@@ -187,23 +190,36 @@ def download_terra_incognita(data_dir):
     ]
 
     images_folder = os.path.join(full_path, "eccv_18_all_images_sm/")
-    annotations_file = os.path.join(full_path, "caltech_images_20210113.json")
+    annotations_folder = os.path.join(full_path,"eccv_18_annotation_files/")
+    cis_test_annotations_file = os.path.join(full_path, "eccv_18_annotation_files/cis_test_annotations.json")
+    cis_val_annotations_file =   os.path.join(full_path, "eccv_18_annotation_files/cis_val_annotations.json")
+    train_annotations_file =   os.path.join(full_path, "eccv_18_annotation_files/train_annotations.json")
+    trans_test_annotations_file =   os.path.join(full_path, "eccv_18_annotation_files/trans_test_annotations.json")
+    trans_val_annotations_file =   os.path.join(full_path, "eccv_18_annotation_files/trans_val_annotations.json")
+    annotations_file_list = [cis_test_annotations_file, cis_val_annotations_file, train_annotations_file, trans_test_annotations_file, trans_val_annotations_file]
     destination_folder = full_path
 
     stats = {}
+    data = defaultdict(list)
 
     if not os.path.exists(destination_folder):
         os.mkdir(destination_folder)
 
-    with open(annotations_file, "r") as f:
-        data = json.load(f)
+    for annotations_file in annotations_file_list:
+        annots = {}
+        with open(annotations_file, "r") as f:
+            annots = json.load(f)
+            for k, v in annots.items():
+                data[k].extend(v)
+
+
 
     category_dict = {}
     for item in data['categories']:
         category_dict[item['id']] = item['name']
 
     for image in data['images']:
-        image_location = image['location']
+        image_location = str(image['location'])
 
         if image_location not in include_locations:
             continue
@@ -243,7 +259,8 @@ def download_terra_incognita(data_dir):
                 shutil.copyfile(src_path, dst_path)
 
     shutil.rmtree(images_folder)
-    os.remove(annotations_file)
+    shutil.rmtree(annotations_folder)
+
 
 
 # SVIRO #################################################################
